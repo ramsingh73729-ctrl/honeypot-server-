2a8ca7db4c903b84d63313e6a28add723900a19f
Ram Singh <ramsingh73729@gmail.com>
2026-09-10 05:55:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
-const express = require('express');
-const app = express(); // Express app initialize karein
-
-// Aapka baaki ka code, routes aur middlewares yahan aayenge...
-
-// Server ko Render ke dynamic port par listen karne ke liye set karein
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Honeypot server is running and live!`);
 });
